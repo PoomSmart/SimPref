40cb65082cc1e5c0301ef4776027799327f6630a
Thatchapon Unprasert <poomst@hotmail.com>
2017-07-07 21:20:04 +0700

    Unsandboxed path.

--- Pref.h
+++ Pref.h
@@ -1,5 +1,5 @@
 #if TARGET_OS_SIMULATOR
-#define realPrefPath(domain) [NSString stringWithFormat:@"%@/Library/Preferences/%@.plist", NSHomeDirectory(), domain]
+#define realPrefPath(domain) [NSString stringWithFormat:@"%@/Library/Preferences/%@.plist", @(getenv("SIMULATOR_SHARED_RESOURCES_DIRECTORY")), domain]
 #else
 #define realPrefPath(domain) [NSString stringWithFormat:@"/User/Library/Preferences/%@.plist"]
 #endif
